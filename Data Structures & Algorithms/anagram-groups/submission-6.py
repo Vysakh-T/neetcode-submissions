1275c440b6cbf3c7490d31f8922c64c5c1e54531
Vysakh T <53861783+Vysakh-T@users.noreply.github.com>
2026-08-18 15:53:06 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        for i in range(len(strs)):
+            ssorted = tuple(sorted(strs[i]))
+            if seen.get(ssorted)==None:
+                seen[ssorted]=[strs[i]]
+            else:
+                seen[ssorted].append(strs[i])
+            
+        return list(seen.values())
